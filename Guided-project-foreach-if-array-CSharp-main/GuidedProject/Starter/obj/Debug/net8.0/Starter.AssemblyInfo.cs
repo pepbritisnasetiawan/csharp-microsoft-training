74da498909ec3efb51b3f220cd84c1114f21c788
pepbritisnasetiawan <pepbritisnasetiawan@gmail.com>
2024-06-13 11:26:00 +0700

    edit

--- Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+347aaff6f88ec77bfa86d10bc06cad9b2a553fc6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f0dee19788bafdfcfe442a2111cb904ebcb1cf5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
