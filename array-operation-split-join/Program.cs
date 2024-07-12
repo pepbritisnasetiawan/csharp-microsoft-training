8da6c0b5814e75102c7c427ae1b7e9f0d8b51de1
pepbritisnasetiawan <pepbritisnasetiawan@gmail.com>
2024-07-13 02:11:17 +0700

    use ToCharArray, Split, join

---
+++ array-operation-split-join/Program.cs
@@ -0,0 +1,19 @@
+﻿string value = "abc123";
+char[] valueArray = value.ToCharArray();
+Array.Reverse(valueArray);
+string result = new string(valueArray);
+string result2 = String.Join(",", valueArray);
+Console.WriteLine(result);
+Console.WriteLine(result2);
+
+string[] items = result2.Split(',');
+foreach (string item in items)
+{
+    Console.WriteLine(item);
+}
+
+/*
+Use method like ToCharArray() adn Split() to create an array
+Use method like join(), or create a new string passing in an array of char
+to turn the array back into single string
+*/
